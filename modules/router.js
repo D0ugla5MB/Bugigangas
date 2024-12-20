68b15aba3c1896de95bc0d85c3e4700181a5bfbc
DMB <douglasmbdev@gmail.com>
2024-12-20 16:46:24 -0300

    preparing router config

--- modules/router.js
+++ modules/router.js
@@ -1,3 +1,28 @@
-const routes = {
-    '/': () => renderHome()
-};
+export function changeRoute(route) {
+	window.location.hash = route;
+}
+
+export function loadContent() {
+	const hash = window.location.hash || '#home';
+	const contentDiv = document.getElementById('content');
+
+	switch (hash) {
+		case '#home':
+			fetch('/home/home.html').then(response => response.text()).then(html => {
+				contentDiv.innerHTML = html;
+			});
+			break;
+			default:
+				fetch('/error.html').then(response => response.text()).then(html => {
+				contentDiv.innerHTML = html;
+			});
+			break;
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	loadContent();
+
+	window.addEventListener('hashchange', loadContent);
+	window.changeRoute = changeRoute;
+});
