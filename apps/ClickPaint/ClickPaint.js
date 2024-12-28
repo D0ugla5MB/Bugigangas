b59e6fb7d7735132923b5e775145e4a93ae85fb0
DMB <douglasmbdev@gmail.com>
2024-12-27 22:19:59 -0300

    the coords system is working; working at resize

--- apps/ClickPaint/ClickPaint.js
+++ apps/ClickPaint/ClickPaint.js
@@ -29,24 +29,6 @@ const timer = setInterval(controller, 500);
 })();
 /*--------------------------------------__SEPARATING FUNCTIONAL PARTS__--------------------------------*/
 
-const svgContainer = (w, h) => {
-    const mainContainer = document.getElementById('app-root');
-    const elem = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    elem.setAttributeNS(null, 'id', 'paint-area');
-    elem.setAttributeNS(null, 'width', `${w}`);
-    elem.setAttributeNS(null, 'height', `${h}`);
-    elem.setAttributeNS(null, 'viewBox', `0 0 ${w} ${h}`);
-    return elem;
-}
-
-const circle = (cx, cy, r, style) => {
-    const elem = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    elem.setAttributeNS(null, 'r', `${r}`);
-    elem.setAttributeNS(null, 'cx', `${cx}`);
-    elem.setAttributeNS(null, 'cy', `${cy}`);
-    elem.setAttributeNS(null,'fill', `${style}`);
-    return elem;
-}
 
 function generateColor() {
     /*  d = [(255 - r)^2 + (255 - g)^2 + (255 - b)^2]^0.5 
@@ -70,35 +52,65 @@ function generateColor() {
     return `#${r.toString(16).padStart(2, "0")}${g.toString(16).padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
 }
 
-function paint(elem) {
-    console.log(elem);
+function watchPointer(event) {
+    return [event.offsetX, event.offsetY];
+}
+
+const circle = (cx, cy, r, style) => {
+    const elem = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    elem.setAttributeNS(null, 'r', `${r}`);
+    elem.setAttributeNS(null, 'cx', `${cx}`);
+    elem.setAttributeNS(null, 'cy', `${cy}`);
+    elem.setAttributeNS(null, 'fill', `${style}`);
     return elem;
 }
 
-function watchPointer(event) {
-    return [event.clientX, event.clientY];
+function getViewportDimensions() {
+    return {
+        width: window.innerWidth || window.visualViewport.width,
+        height: window.innerHeight || window.visualViewport.height
+    }
+}
+
+function getSvgContainerSide(viewportDim) {
+    return viewportDim.width > viewportDim.height ? viewportDim.height : viewportDim.width;
+}
+
+const svgContainer = (containerSide) => {
+    const elem = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    elem.setAttributeNS(null, 'id', 'paint-area');
+    elem.setAttributeNS(null, 'width', `${containerSide}`);
+    elem.setAttributeNS(null, 'height', `${containerSide}`);
+    elem.setAttributeNS(null, 'viewBox', `0 0 ${containerSide} ${containerSide}`);
+    elem.setAttributeNS(null, 'aspect-ration', '1');
+    return elem;
+}
+
+function buildPaintArea() {
+    return svgContainer('' + getSvgContainerSide(getViewportDimensions()));
 }
 
 (function runApp() {
-    const main = document.getElementById('app-root');
-    const svgArea = svgContainer('500', '500');
-    main.appendChild(svgArea);
-
+    const mainContainer = document.getElementById('app-root');
     let clicked = false;
-    const paintArea = document.getElementById('paint-area');
-    
+    let paintArea = buildPaintArea();
+
+    if (!paintArea) throw console.error('Something wrong happened');
+
+    mainContainer.appendChild(paintArea);
 
     paintArea.addEventListener('click', () => {
         clicked = true;
         console.log('clicked');
-        
+
     });
     paintArea.addEventListener('mousemove', (event) => {
         if (clicked) {
             clicked = false;
             const [cx, cy] = watchPointer(event);
+            console.log(`${cx}, ${cy}`);
             paintArea.appendChild(circle(cx, cy, 25, generateColor()))
         }
     });
-    
+
 })();
