3fba2012308bffb8b41df96f71adc8d3f2e8f173
DMB <douglasmbdev@gmail.com>
2025-02-11 23:32:44 -0300

    feat: implement word seek game with grid system and interaction mechanics

---
+++ scripts/apps/word-seek/constants.js
@@ -0,0 +1,7 @@
+const GRID_CELL = document.createElement('span');
+const GRID_LINE = document.createElement('div');
+const GRID = document.createElement('section');
+const WORD_SCORE = document.createElement('div');
+const LINES_SET = [];
+const WORD_LIST = {};
+const ARROWS_EQUATION = [];
