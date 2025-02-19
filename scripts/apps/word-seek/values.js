53885e147bf9de4e8a54a9f36d9256eac7d0c517
DMB <douglasmbdev@gmail.com>
2025-02-19 20:47:44 -0300

    new: v3.2.1

--- scripts/apps/word-seek/values.js
+++ scripts/apps/word-seek/values.js
@@ -1,20 +1,45 @@
-const MAIN_CONTAINER = Object.freeze('main');
-const TEXT = Object.freeze('p');
-const GRID_CELL = Object.freeze('span');
-const GRID_LINE = Object.freeze('div');
-const GRID = Object.freeze('section');
-const CATEGORY_MENU = Object.freeze('section');
-const CATEGORY_MENU_BTN = Object.freeze('btn');
-const CATEGORY_MENU_OPTIONS = Object.freeze('nav');
-const WORD_COUNT = Object.freeze('section');
-const WORD_COUNT_FRACTION = Object.freeze('div');
-const WORD_COUNT_CURRENT = Object.freeze('span');
-const WORD_COUNT_TOTAL = Object.freeze('span');
-const WORD_COUNT_DIVISION_BAR = Object.freeze('span');
+export const EQUATIONS = Object.freeze({
+    calcSafeLimit: SAFE_GRID_SIZE_EQ.CALC_SAFE_LIMIT,
+    directions: DIRECTIONS,
+    findIndex: BASE_INDEX_EQUATION.FIND_INDEX,
+    arrowsQuantity: ARROWS_QUANTITY.arrows_n,
+});
+
+export const BUILDING = Object.freeze({
+    gridCell: GRID_CELL,
+    gridLine: GRID_LINE,
+    gridContainer: GRID,
+    wordCount: WORD_COUNT,
+    linesSet: LINES_SET,
+    colorCorrect: COLOR_CORRECT,
+    colorIncorrect: COLOR_INCORRECT,
+    units: UNITS,
+});
+
+export const EVENTS = Object.freeze({
+    clickToClick: EVENTS_APPROACHES.CLICK_TO_CLICK,
+    dragSelection: EVENTS_APPROACHES.DRAG_SELECTION,
+    manualSelection: EVENTS_APPROACHES.MANUAL_SELECTION,
+});
+
+export const VALUES = Object.freeze({
+    strAux: STR_AUX,
+    gameModes: GAME_MODES,
+    wordsCategoryList: WORDS_CATEGORY_LIST,
+    arrowsEqList: ARROWS_EQUATIONS,
+    gridDefaultSize: GRID_DEFAULT_SIZE,
+});
+
+export const DATA_SOURCE = Object.freeze({
+
+});
+
+const GRID_CELL = document.createElement('span');
+const GRID_LINE = document.createElement('div');
+const GRID = document.createElement('section');
+const WORD_COUNT = document.createElement('div');
 const LINES_SET = Object.freeze([]);
-const WORDS_CATEGORY_LIST = Object.freeze([
-    'Animals', 'Objects'
-]);
+const WORDS_CATEGORY_LIST = Object.freeze([]);
 const BASE_INDEX_EQUATION = Object.freeze({
     FIND_INDEX: (arr_len, sub_arr_len, sub_arr_r, sub_arr_c) =>
         (Math.floor(arr_len / sub_arr_len)) * sub_arr_r + sub_arr_c
@@ -58,55 +83,6 @@ const GAME_MODES = Object.freeze({
 });
 
 const GRID_DEFAULT_SIZE = [10, 15, 20];
-
-export const EQUATIONS = Object.freeze({
-    calcSafeLimit: SAFE_GRID_SIZE_EQ.CALC_SAFE_LIMIT,
-    directions: DIRECTIONS,
-    findIndex: BASE_INDEX_EQUATION.FIND_INDEX,
-    arrowsQuantity: ARROWS_QUANTITY.arrows_n,
-});
-
-export const BUILDING = Object.freeze({
-    Grid: {
-        gridCell: GRID_CELL,
-        gridLine: GRID_LINE,
-        gridContainer: GRID,
-    },
-    WordCounter: {
-        counter: WORD_COUNT,
-        current: WORD_COUNT_CURRENT,
-        total: WORD_COUNT_TOTAL,
-        fraction: WORD_COUNT_FRACTION,
-        fracBar: WORD_COUNT_DIVISION_BAR
-    },
-    Word: {
-        colorCorrect: COLOR_CORRECT,
-        colorIncorrect: COLOR_INCORRECT,
-    },
-    CategoryMenu: {
-        menuBase: CATEGORY_MENU,
-        menuBtn: CATEGORY_MENU_BTN,
-        menuOpt: CATEGORY_MENU_OPTIONS
-    },
-    Text: TEXT,
-    linesSet: LINES_SET,
-    units: UNITS,
-    main: MAIN_CONTAINER,
-});
-
-export const EVENTS = Object.freeze({
-    clickToClick: EVENTS_APPROACHES.CLICK_TO_CLICK,
-    dragSelection: EVENTS_APPROACHES.DRAG_SELECTION,
-    manualSelection: EVENTS_APPROACHES.MANUAL_SELECTION,
-});
-
-export const VALUES = Object.freeze({
-    strAux: STR_AUX,
-    gameModes: GAME_MODES,
-    wordsCategoryList: WORDS_CATEGORY_LIST,
-    arrowsEqList: ARROWS_EQUATIONS,
-    gridDefaultSize: GRID_DEFAULT_SIZE,
-});
 
 export default {
     EQUATIONS,
